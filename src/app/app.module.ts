a327098224092c9abfcdd342f25dd646eaee5dd7
Yurii Haideichuk <yuriy.haydeychuk@gmail.com>
2020-02-19 23:34:20 +0200

    init ngrx effects and tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
 // Modules
 import { MaterialModule } from './modules/material.module';
 
 // Store
 import { reducers, metaReducers } from './store/reducers';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +31,11 @@ import { StoreModule } from '@ngrx/store';
     BrowserAnimationsModule,
     MaterialModule,
     StoreModule.forRoot(reducers, { metaReducers, runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true } }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
