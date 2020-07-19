2a0c8d5be2435b64fcae02e455928227cabd6c60
BulletproofUA <yuriy.haydeychuk@gmail.com>
2020-07-19 17:08:16 +0300

    * fix deploy setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAV-0UzHqy8gpLG0Z7FRPPfoWDJnKrX26w",
+    authDomain: "udrg-vascular-16f0c.firebaseapp.com",
+    databaseURL: "https://udrg-vascular-16f0c.firebaseio.com",
+    projectId: "udrg-vascular-16f0c",
+    storageBucket: "udrg-vascular-16f0c.appspot.com",
+    messagingSenderId: "641731472593",
+    appId: "1:641731472593:web:c91f5ba7b2e7661dbf69f0"
+  }
 };
